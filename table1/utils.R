cba1f54b8c2c35bda86f13454e355731ce771015
fernandoprudencio <fjprudenciop@gmail.com>
2021-05-24 09:51:49 -0500

    sparcs dataset was added

--- table1/utils.R
+++ table1/utils.R
@@ -1,5 +1,4 @@
-function(dataset, path) {
-  path <- "dataset/irish"
+countlabeling <- function(dataset, path) {
   if (dataset == "irish") {
     # list of scenes
     list.img <-
@@ -24,58 +23,112 @@ function(dataset, path) {
         p255 = numeric(),
         id = character(),
       )
-  }
-  # fill table
-  for (i in seq_along(list.img)) { # seq_along(list.img)
-    cat(sprintf("scene = %1s\n", i))
-    options(warn = -1)
-    ee_img <- ee$Image(sprintf("LANDSAT/LE07/C01/T2_TOA/%1$s", id[i]))
-    # if image not exist, runs it
-    if (
-      grepl(
-        try(
-          ee_get_date_img(ee_img),
-          silent = TRUE
-        ),
-        pattern = "Error"
-      ) %>% unique()
-    ) {
-      ee_img <- ee$Image(sprintf("LANDSAT/LE07/C01/T1/%1$s", id[i]))
-    }
-    # load raster
-    img <- raster(list.img[i])
-    # load tile
-    tile <-
-      ee_img$
-        geometry() %>%
-      ee_as_sf() %>%
-      st_transform(crs = st_crs(img)) %>%
-      sf::st_buffer(-2500)
-    # mask raster
-    img %<>% raster::mask(mask = tile)
-    # build table
-    df %<>%
-      bind_rows(
-        t(
-          data.frame(
-            getValues(img) %>%
-              table()
-          )
-        ) %>%
-          as_data_frame() %>%
-          janitor::row_to_names(row_number = 1) %>%
-          rename_all(~ sprintf("p%1s", .x)) %>%
-          mutate_all(as.numeric) %>%
-          mutate(
-            valid = rowSums(
-              across(where(is.numeric)),
-              na.rm = T
-            ),
-            invalid = ncell(img) - valid,
-            total = ncell(img)
+    # fill table
+    for (i in seq_along(list.img)) {
+      cat(sprintf("scene = %1s\n", i))
+      options(warn = -1)
+      ee_img <- ee$Image(sprintf("LANDSAT/LE07/C01/T2_TOA/%1$s", id[i]))
+      # if image not exist, runs it
+      if (
+        grepl(
+          try(
+            ee_get_date_img(ee_img),
+            silent = TRUE
+          ),
+          pattern = "Error"
+        ) %>% unique()
+      ) {
+        ee_img <- ee$Image(sprintf("LANDSAT/LE07/C01/T1/%1$s", id[i]))
+      }
+      # load raster
+      img <- raster(list.img[i])
+      # load tile
+      tile <-
+        ee_img$
+          geometry() %>%
+        ee_as_sf() %>%
+        st_transform(crs = st_crs(img)) %>%
+        sf::st_buffer(-2500)
+      # mask raster
+      img %<>% raster::mask(mask = tile)
+      # build table
+      df %<>%
+        bind_rows(
+          t(
+            data.frame(
+              getValues(img) %>%
+                table()
+            )
           ) %>%
-          mutate(id = id[i])
+            as_data_frame() %>%
+            janitor::row_to_names(row_number = 1) %>%
+            rename_all(~ sprintf("p%1s", .x)) %>%
+            mutate_all(as.numeric) %>%
+            mutate(
+              valid = rowSums(
+                across(where(is.numeric)),
+                na.rm = T
+              ),
+              invalid = ncell(img) - valid,
+              total = ncell(img)
+            ) %>%
+            mutate(id = id[i])
+        )
+    }
+  } else if (dataset == "sparcs") {
+    path <- "dataset/sparcs"
+    # List of raster
+    lst <-
+      list.files(
+        path,
+        pattern = "\\mask.png$",
+        full.names = T,
+        recursive = T
       )
+    # List of scene id
+    id <- basename(lst) %>% str_sub(1, -5)
+    # Build table ----
+    # create empty table
+    df <-
+      tibble(
+        p0 = numeric(),
+        p1 = numeric(),
+        p2 = numeric(),
+        p3 = numeric(),
+        p4 = numeric(),
+        p5 = numeric(),
+        p6 = numeric(),
+        id = character()
+      )
+    # fill table
+    for (i in seq_along(lst)) {
+      cat(sprintf("scene = %1s\n", i))
+      # load raster
+      img <- raster(lst[i])
+      # build table
+      df %<>%
+        bind_rows(
+          t(
+            data.frame(
+              getValues(img) %>%
+                table()
+            )
+          ) %>%
+            as_tibble() %>%
+            janitor::row_to_names(row_number = 1) %>%
+            rename_all(~ sprintf("p%1s", .x)) %>%
+            mutate_all(as.numeric) %>%
+            mutate(
+              valid = rowSums(
+                across(where(is.numeric)),
+                na.rm = T
+              ),
+              invalid = ncell(img) - valid,
+              total = ncell(img)
+            ) %>%
+            mutate(id = id[i])
+        )
+    }
   }
   # change colnames
   if (dataset == "irish") {
@@ -84,6 +137,14 @@ function(dataset, path) {
         "Fill", "Cloud_Shadow", "Clear", "Thin_Cloud",
         "Cloud", "id", "Valid", "Invalid", "Total"
       )
-    mutate_all(df, ~ replace(., is.na(.), 0)) %>% return()
+  } else if (dataset == "sparcs") {
+    names(df) <-
+      c(
+        "Shadow", "Shadow_over_Water", "Water",
+        "Snow", "Land", "Cloud", "Flooded", "id",
+        "Valid", "Invalid", "Total"
+      )
   }
+  # return table
+  mutate_all(df, ~ replace(., is.na(.), 0)) %>% return()
 }
